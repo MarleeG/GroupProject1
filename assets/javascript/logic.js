b8c0d4740194a15f3221af2e7d169e80fdd5ef3b
Cameron Ashlyn <33036729+CameronAshlyn@users.noreply.github.com>
2018-07-08 20:16:04 -0400

    adds MDB setup and initial UI elements

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,1 +1,23 @@
-<script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
+$(function () {
+    
+    // Navbar Icon
+    $('.animated-icon1').click(function(){
+        $(this).toggleClass('open',);
+    });
+
+   
+
+// click event to launch representatives page
+$("#launchBtn").on("click", function (event) {
+
+    //prevents page from refreshing on the click of the start button
+    event.preventDefault();
+    
+    // hides start button after it is clicked
+    $(this).css("display", "none", "background-color", "white");
+    // $("#titlePage").css("display", "none");
+    // $("#myReps").css("display", "block");
+
+    });
+});
+    
