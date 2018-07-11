17cbe284cf27aa3c6c8cadd1dfef56ae906f6af7
Jonathan Yi <jyi428@Jonathans-MacBook-Pro.local>
2018-07-11 12:59:20 -0400

    test

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -47,3 +47,87 @@ $.ajax({
         }
     });
    
+    var allStates = {
+        Alabama: "AL",
+        Alaska: "AK",
+        Arizona: "AZ",
+        Arkansas: "AR",
+        California: "CA",
+        Colorado: "CO",
+        Connecticut: "CT",
+        Delaware: "DE",
+        Florida: "FL",
+        Georgia: "GA",
+        Hawaii: "HI",
+        Idaho: "ID",
+        Illinois: "IL",
+        Indiana: "IN",
+        Iowa: "IA",
+        Kansas: "KS",
+        Kentucky: "KY",
+        Lousiana: "LA",
+        Maine: "ME",
+        Maryland: "MD",
+        Massachusetts: "MA",
+        Michigan: "MI",
+        Minesota: "MN",
+        Missippi: "MS",
+        Missouri: "MO",
+        Montana: "MT",
+        Nebraska: "NE",
+        Nevada: "NV",
+        New_Jersey: "NJ",
+        New_Hempshire: "NH",
+        New_Mexico: "NM",
+        New_York: "NY",
+        North_Carolina: "NC",
+        North_Dakota: "ND",
+        Ohio: "OH",
+        Oklahoma: "OK",
+        Oregon: "OR",
+        Pennsylvania: "PA",
+        Rhode_Island: "RI",
+        South_Carolina: "SC",
+        South_Dakota: "SD",
+        Tennessee: "TN",
+        Texas: "TX",
+        Utah: "UT",
+        Vermont: "VT",
+        Virginia: "VA",
+        Washington: "WA",
+        West_Virginia: "WV",
+        Wisconsin: "WI",
+        Wyoming: "WY"
+    }
+
+    var x = document.getElementById("states").selectedIndex;
+    var select = document.getElementsByTagName("option")[x].value;
+
+    switch (select) {
+        case "New Jersey": value = allStates.New_Jersey;
+            break;
+        case "New Hempshire": value = allStates.New_Hempshire;
+            break;
+        case "New Mexico": value = allStates.New_Mexico;
+            break;
+        case "New York": value = allStates.New_York;
+            break;
+        case "North Carolina": value = allStates.North_Carolina;
+            break;
+        case "North Dakota": value = allStates.North_Dakota;
+            break;
+        case "Rhode Island": value = allStates.Rhode_Island;
+            break;
+        case "South Carolina": value = allStates.South_Carolina;
+            break;
+        case "South Dakota": value = allStates.South_Dakota;
+            break;
+        case "South Dakota": value = allStates.South_Dakota;
+            break;
+        case "West Virginia": value = allStates.West_Virginia;
+            break;
+        default: value = allStates[select];
+    }
+
+
+Add Comment
